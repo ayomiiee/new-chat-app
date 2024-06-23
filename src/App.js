0ac4ab7fc091af2974169da76ad51b1f3d4553b7
ayomiiee <ayomialakija3@gmail.com>
2024-06-23 13:03:14 +0100

    commit all

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import io, { Socket } from 'socket.io-client'
+import Chat from './chat';
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+const [username, setUsername] = useState("");
+const[room, setRoom] = useState("");
+
+const joinRoom = () => {
+  if(username !== "" && room !== "") {
+    socket.emit("join_room", room);
+  }
+};
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+    <h3> Join A Chat </h3>
+     <input  type="text"
+      placeholder="Lateef..." 
+      onChnage= {(event) => {
+        setUsername(event.target.value);
+        }}
+        />
+     <input  type="text" 
+     placeholder="Room ID..."
+     onChnage= {(event) => {
+      setRoom(event.target.value);
+      }}
+     />
+     <button onClick={joinRoom}> Join A Room </button>
+
+    <Chat socket={socket} username={username} room={room} />
+  </div>
   );
 }
 
---
+++ src/chat.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+
+function Chat ({socket, username, room})  {
+    const [currentMessage, setCurrentMessage] = useState("");
+
+    const sendMessage = async () => {
+        if (currentMessage !== "") {
+            const messageData = {
+                room: room,
+                authur: username,
+                message: currentMessage,
+                time: new Date(Date.now()).getHours() +
+                 ":"  + 
+                 new Date(Date.now()).getMinutes(),
+            };
+
+            await socket.emit("send_mesaage, mesageData");
+        }
+    };
+    return (
+        <div>
+            <div  className="chat-header" >
+                <p> Live Chat </p>
+            </div>
+            <div  className="chat-body" ></div>
+            <div  className="chat-footer" >
+                <input type="text" 
+                placeholder="Hey..."
+                onChnage= {(event) => {
+                    setCurrentMessage(event.target.value);
+                    }}
+                />
+                <button onClick={sendMessage}>&#9658;</button>
+            </div>
+        </div>
+    )
+}
+
+export default Chat
